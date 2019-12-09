579f7f05d3835ea317b064aab44131f1b379ac53
Radiant1189 <58019177+Radiant1189@users.noreply.github.com>
2019-12-08 17:45:02 -0700

    Add files via upload
    Added switches into Top and pong files

--- pong.vhd
+++ pong.vhd
@@ -7,6 +7,7 @@ entity pong_graph_st is
 	port(
 	clk, reset: in std_logic;
 	btn:in std_logic_vector(3 downto 0);
+	switch: in std_logic_vector(3 downto 1);
 	video_on:in std_logic;
 	pixel_x:in unsigned(9 downto 0);
 	pixel_y:in unsigned(9 downto 0);
@@ -24,8 +25,8 @@ architecture sq_ball_arch of pong_graph_st is
    constant MAX_X: integer := 640;
    constant MAX_Y: integer := 480;
 
-   constant WALL_X_L: integer := 32;
-   constant WALL_X_R: integer := 35;
+constant WALL_X_L: integer := 32;
+constant WALL_X_R: integer := 35;
 
 -- For right bar
    constant BAR1_X_L: integer := 600;
@@ -35,16 +36,21 @@ architecture sq_ball_arch of pong_graph_st is
    constant BAR1_YH_SIZE: integer := 31;
 
 -- Variable for general score
-   signal xp_score_val: unsigned(6 downto 0);
+   signal xp_score1_val: unsigned(6 downto 0);
+   signal xp_score2_val: unsigned(6 downto 0);
    
--- To track player 2 score
+-- To track player 1 score
    signal score1_reg, score1_next: unsigned(6 downto 0);
-   signal score1_add_reg, score1_add_next: unsigned(6 downto 0);
+--   signal score1_add_reg, score1_add_next: unsigned(6 downto 0);
+--   signal score1: std_logic;
    --signal SCORE_UP: integer := 1;
+   signal win_score1_trig: std_logic;
 
 -- To track player 2 score
    signal score2_reg, score2_next: unsigned(6 downto 0);
-   signal score2_add_reg, score2_add_next: unsigned(6 downto 0);
+--   signal score2_add_reg, score2_add_next: unsigned(6 downto 0);
+--   signal score2: std_logic;
+   signal win_score2_trig: std_logic;
    
 -- Adding / Subtracting scores
    constant SCORE_UP: unsigned(6 downto 0) := to_unsigned(1,7);
@@ -72,6 +78,7 @@ architecture sq_ball_arch of pong_graph_st is
    -- reg to track left and top boundary
    signal ball_x_reg, ball_x_next: unsigned(9 downto 0);
    signal ball_y_reg, ball_y_next: unsigned(9 downto 0);
+   signal center: std_logic;
 
    signal x_delta_reg, x_delta_next:
       unsigned(9 downto 0);
@@ -301,10 +308,15 @@ architecture sq_ball_arch of pong_graph_st is
         "0000000000000000");
    
         
-   -- For score generation
+   -- For score1 generation
    signal rom_addrN, rom_colN: unsigned(3 downto 0);
    signal rom_dataN: std_logic_vector(0 to 15);
    signal rom_bitN: std_logic;
+   
+   --For score2 generation
+   signal rom_addrN2, rom_colN2: unsigned(3 downto 0);
+   signal rom_dataN2: std_logic_vector(0 to 15);
+   signal rom_bitN2: std_logic;
    
    --tiles for score
    signal p1_x_l, p1_x_r: unsigned(9 downto 0);
@@ -330,9 +342,6 @@ architecture sq_ball_arch of pong_graph_st is
    		unsigned(to_signed(0,10)); 
    signal word_score_on, sq_word_on: std_logic;
    
-   -- signal for the score
-   signal score_reg, score_next: 
-        unsigned(6 downto 0);
       
 
 
@@ -351,9 +360,9 @@ architecture sq_ball_arch of pong_graph_st is
    		   bar2_y_reg <= (others => '0');
    		   score1_reg <= (others => '0');
    		   score2_reg <= (others => '0');
-   		   score1_add_reg <= (others => '0');
-   		   score2_add_reg <= (others => '0');
-   		   ball_x_reg <= (others => '0');
+--   		   score1_add_reg <= (others => '0');
+--   		   score2_add_reg <= (others => '0');
+   		   ball_x_reg <= "0100111111";
    		   ball_y_reg <= (others => '0');
    		   x_delta_reg <= ("0000000100");
    		   y_delta_reg <= ("0000000100");
@@ -362,8 +371,8 @@ architecture sq_ball_arch of pong_graph_st is
            bar2_y_reg <= bar2_y_next;
            score1_reg <= score1_next;
            score2_reg <= score2_next;
-           score1_add_reg <= score1_add_next;
-           score2_add_reg <= score2_add_next;
+--           score1_add_reg <= score1_add_next;
+--           score2_add_reg <= score2_add_next;
            ball_x_reg <= ball_x_next;
            ball_y_reg <= ball_y_next;
            x_delta_reg <= x_delta_next;
@@ -397,13 +406,15 @@ architecture sq_ball_arch of pong_graph_st is
     	(pix_y <= bar2_y_b) else '0';
     --bar_rgb <= "010"; -- blue
 
-    process( bar1_y_reg, bar1_y_b, bar1_y_t, refr_tick, btn)
+    process( bar1_y_reg, bar1_y_b, bar1_y_t, refr_tick, btn, switch)
     	begin
     	bar1_y_next <= bar1_y_reg; -- no move 
 
     	if ( refr_tick = '1' ) then
+    	   if( switch(2) = '1') then
+    	   bar1_y_next <= "0000000001";
     -- if btn 1 pressed and paddle not at bottom yet
-    		if ( btn(1) = '1' and bar1_y_b <
+    	     elsif ( btn(1) = '1' and bar1_y_b <
     			(MAX_Y - 1 - BAR_V)) then
     			bar1_y_next <= bar1_y_reg + BAR_V;
 	-- if btn 0 pressed and bar not at top yet
@@ -418,8 +429,10 @@ end process;
     	bar2_y_next <= bar2_y_reg; -- no move 
 
     	if ( refr_tick = '1' ) then
+    	    if( switch(2) = '1' ) then
+    	    bar2_y_next <= "0000000001";
     -- if btn 1 pressed and paddle not at bottom yet
-    		if ( btn(3) = '1' and bar2_y_b <
+    		elsif ( btn(3) = '1' and bar2_y_b <
     			(MAX_Y - 1 - BAR_V)) then
     			bar2_y_next <= bar2_y_reg + BAR_V;
 	-- if btn 0 pressed and bar not at top yet
@@ -454,39 +467,57 @@ end process;
 			(rom_bit = '1') else '0';
 	  ball_rgb <= "100"; -- green
 
-	  -- Update the ball position 60 times per second.
-	  ball_x_next <= ball_x_reg + x_delta_reg when
-			 refr_tick = '1' else ball_x_reg;
-	  ball_y_next <= ball_y_reg + y_delta_reg when
-			 refr_tick = '1' else ball_y_reg;
+	  -- Update the ball position 60 times per second, hold when switch 1, reset when switch "11"
+	  ball_x_next <= ball_x_reg               when switch(1) = '1'  else
+	                 "0100111111"             when center = '1'     else
+	                 ball_x_reg + x_delta_reg when refr_tick = '1'  else
+	                 ball_x_reg;
+	  ball_y_next <= ball_y_reg               when switch(1) = '1'  else
+	                 "0000000000"             when center = '1'     else
+	                 ball_y_reg + y_delta_reg when refr_tick = '1'  else 
+	                 ball_y_reg;
 			 
       --Keep track of each player's score
-      score1_next <= score1_reg + score1_add_reg when 
+--      score1_next <= score1_reg + 1 when score1 = '1' else 
+--                     score1_reg; 
              
-      score2_next <= score2_reg + score2_add_reg;
+--      score2_next <= score2_reg + 1 when score2 = '1' else
+--                     score2_reg;
        
 	  -- Set the value of the next ball position according to
 	  -- the boundaries.
-	  process(x_delta_reg, y_delta_reg, score1_add_reg, ball_y_t, ball_x_l, ball_x_r, ball_y_t, ball_y_b, bar1_y_t, bar1_y_b, bar2_y_t, bar2_y_b)
+	  process(x_delta_reg, y_delta_reg, score1_reg, score2_reg, ball_y_t, ball_x_l, ball_x_r, ball_y_t, ball_y_b, bar1_y_t, bar1_y_b, bar2_y_t, bar2_y_b, center)
 		  begin
 		  x_delta_next <= x_delta_reg;
 		  y_delta_next <= y_delta_reg;
-		  score1_add_next <= "0000000";
-		  score2_add_next <= "0000000";
+		  score1_next <= score1_reg;
+		  score2_next <= score2_reg;
+		  center <= '0';
+		  win_score1_trig <= '0';
+		  win_score2_trig <= '0';
+--		  score1 <= '0'; score2 <= '0';
 	  -- ball reached top, make offset positive
-		  if ( ball_y_t < 1 ) then
+	      if ((score1_reg >= 10) or (score2_reg >= 10)) then
+	           if (score1_reg > 10) then
+	               win_score1_trig <= '1';
+	           elsif (score2_reg <= 10) then
+	               win_score2_trig <= '1';
+	           end if;
+		  elsif ( ball_y_t < 1 ) then
 			  y_delta_next <= BALL_V_P;
 	  -- reached bottom, make negative
 		  elsif (ball_y_b > (MAX_Y - 1)) then
 			  y_delta_next <= BALL_V_N;
-	  -- Reached right, rebound.
-	      elsif (ball_x_r > (MAX_X - 1)) then
-	          x_delta_next <= BALL_V_N;
-	          score1_add_next <= SCORE_UP;
-	  -- Reached left, rebound.
-	      elsif (ball_x_l < 1 ) then
-	          x_delta_next <= BALL_V_P;
-	          score2_add_next <= SCORE_UP;
+	  -- Reached right, player one scores, center ball
+	      elsif (ball_x_r > 630) then
+	         -- score1_add_next <= SCORE_UP;
+	          center <= '1';
+	          score1_next <= score1_reg + SCORE_UP;
+	  -- Reached left, player two scores, center ball
+	      elsif (ball_x_l <= 20 ) then
+--	          score2_add_next <= SCORE_UP;
+	          center <= '1';
+	          score2_next <= score2_reg + SCORE_UP;
 	  -- left corner of ball inside bar2,
 		  elsif (( ball_x_l <= BAR2_X_R ) and 
 		  (BAR2_X_L <= ball_x_l)) then
@@ -497,7 +528,7 @@ end process;
 		           y_delta_next <= BALL_V_N;
 		       elsif ((bar2_y_m <= ball_y_b) and
 		      (ball_y_t <= bar2_y_b)) then
-		          x_delta_next <= BALL_V_N;
+		          x_delta_next <= BALL_V_P;
 		          y_delta_next <= BALL_V_P;
 		  end if;
 	  -- right corner of ball inside bar1
@@ -506,11 +537,11 @@ end process;
 	  -- some portion of ball hitting paddle, Upper half of bar or lower half of bar
 		      if ((bar1_y_t <= ball_y_b) and
 		      (ball_y_t <= bar1_y_m)) then
-		          x_delta_next <= BALL_V_P;
+		          x_delta_next <= BALL_V_N;
 		          y_delta_next <= BALL_V_N;
 		      elsif ((bar1_y_m <= ball_y_b) and
 		      (ball_y_t <= bar1_y_b)) then
-		          x_delta_next <= BALL_V_P;
+		          x_delta_next <= BALL_V_N;
 		          y_delta_next <= BALL_V_P;
 	      end if;
     end if;
@@ -536,16 +567,17 @@ end process;
 			(pix_y <= p2_y_b) else '0';
 
 -- Logic to display score
-process(p1_score_on, p2_score_on, xp_score_val, score1_next, score2_next)
+process(p1_score_on, xp_score1_val, score1_reg)
             begin
-            xp_score_val <= "0000000";
+            xp_score1_val <= "0000000";
             if(sq_score1_on = '1') then
-                xp_score_val <= score1_next;
+                xp_score1_val <= score1_reg;
             end if;
-            if(sq_score2_on = '1') then
-                xp_score_val <= "0001000";
-            end if;
-            case(xp_score_val) is
+--            if (sq_score2_on = '1') then
+--                xp_score1_val <= score2_reg;
+--                xp_score1_val <= "0000100";
+--            end if;
+            case(xp_score1_val) is
                 when "0000000" =>
                     rom_addrN <= pix_y(3 downto 0);
                     -- ROM column
@@ -636,13 +668,111 @@ process(p1_score_on, p2_score_on, xp_score_val, score1_next, score2_next)
                     rom_bitN <= rom_dataN(to_integer(rom_colN));
             end case;
       end process;
+      
+      process(p2_score_on, xp_score2_val, score2_reg)
+            begin
+            xp_score2_val <= "0000000";
+            if(sq_score2_on = '1') then
+                xp_score2_val <= score2_reg;
+            end if;
+            case(xp_score2_val) is
+                when "0000000" =>
+                    rom_addrN2 <= pix_y(3 downto 0);
+                    -- ROM column
+                    rom_colN2 <= pix_x(3 downto 0);
+                    -- Get row data
+                    rom_dataN2 <= ZERO_ROM(to_integer(rom_addrN2));
+                    -- Get column bit
+                    rom_bitN2 <= rom_dataN2(to_integer(rom_colN2));
+                when "0000001" =>
+                    rom_addrN2 <= pix_y(3 downto 0);
+                    -- ROM column
+                    rom_colN2 <= pix_x(3 downto 0);
+                    -- Get row data
+                    rom_dataN2 <= ONE_ROM(to_integer(rom_addrN2));
+                    -- Get column bit
+                    rom_bitN2 <= rom_dataN2(to_integer(rom_colN2));
+                when "0000010" =>
+                    rom_addrN2 <= pix_y(3 downto 0);
+                    -- ROM column
+                    rom_colN2 <= pix_x(3 downto 0);
+                    -- Get row data
+                    rom_dataN2 <= TWO_ROM(to_integer(rom_addrN2));
+                    -- Get column bit
+                    rom_bitN2 <= rom_dataN2(to_integer(rom_colN2));
+                when "0000011" =>
+                    rom_addrN2 <= pix_y(3 downto 0);
+                    -- ROM column
+                    rom_colN2 <= pix_x(3 downto 0);
+                    -- Get row data
+                    rom_dataN2 <= THREE_ROM(to_integer(rom_addrN2));
+                    -- Get column bit
+                    rom_bitN2 <= rom_dataN2(to_integer(rom_colN2));
+                when "0000100" =>
+                    rom_addrN2 <= pix_y(3 downto 0);
+                    -- ROM column
+                    rom_colN2 <= pix_x(3 downto 0);
+                    -- Get row data
+                    rom_dataN2 <= FOUR_ROM(to_integer(rom_addrN2));
+                    -- Get column bit
+                    rom_bitN2 <= rom_dataN2(to_integer(rom_colN2));
+                when "0000101" =>
+                    rom_addrN2 <= pix_y(3 downto 0);
+                    -- ROM column
+                    rom_colN2 <= pix_x(3 downto 0);
+                    -- Get row data
+                    rom_dataN2 <= FIVE_ROM(to_integer(rom_addrN2));
+                    -- Get column bit
+                    rom_bitN2 <= rom_dataN2(to_integer(rom_colN2));
+                when "0000110" =>
+                    rom_addrN2 <= pix_y(3 downto 0);
+                    -- ROM column
+                    rom_colN2 <= pix_x(3 downto 0);
+                    -- Get row data
+                    rom_dataN2 <= SIX_ROM(to_integer(rom_addrN2));
+                    -- Get column bit
+                    rom_bitN2 <= rom_dataN2(to_integer(rom_colN2));
+                when "0000111" =>
+                    rom_addrN2 <= pix_y(3 downto 0);
+                    -- ROM column
+                    rom_colN2 <= pix_x(3 downto 0);
+                    -- Get row data
+                    rom_dataN2 <= SEVEN_ROM(to_integer(rom_addrN2));
+                    -- Get column bit
+                    rom_bitN2 <= rom_dataN2(to_integer(rom_colN2));
+                when "0001000" =>
+                    rom_addrN2 <= pix_y(3 downto 0);
+                    -- ROM column
+                    rom_colN2 <= pix_x(3 downto 0);
+                    -- Get row data
+                    rom_dataN2 <= EIGHT_ROM(to_integer(rom_addrN2));
+                    -- Get column bit
+                    rom_bitN2 <= rom_dataN2(to_integer(rom_colN2));
+                when "0001001" =>
+                    rom_addrN2 <= pix_y(3 downto 0);
+                    -- ROM column
+                    rom_colN2 <= pix_x(3 downto 0);
+                    -- Get row data
+                    rom_dataN2 <= NINE_ROM(to_integer(rom_addrN2));
+                    -- Get column bit
+                    rom_bitN2 <= rom_dataN2(to_integer(rom_colN2));
+                when others =>
+                    rom_addrN2 <= pix_y(3 downto 0);
+                    -- ROM column
+                    rom_colN2 <= pix_x(3 downto 0);
+                    -- Get row data
+                    rom_dataN2 <= FIVE_ROM(to_integer(rom_addrN2));
+                    -- Get column bit
+                    rom_bitN2 <= rom_dataN2(to_integer(rom_colN2));
+            end case;
+      end process;
     
     -- Turn scores on only if within square and ROM bit is 1.
     p1_score_on <= '1' when (sq_score1_on = '1') and
           (rom_bitN = '1') else '0';
     num1_rgb <= "100"; -- green
     p2_score_on <= '1' when (sq_score2_on = '1') and
-          (rom_bitN = '1') else '0';
+          (rom_bitN2 = '1') else '0';
     num2_rgb <= "001"; -- red
     
   -- Pixel within word score
@@ -672,8 +802,6 @@ process(p1_score_on, p2_score_on, xp_score_val, score1_next, score2_next)
 		  if (video_on = '0') then
 			  graph_rgb <= "000"; -- blank
 	          else
---			  if (wall_on = '1') then
---				  graph_rgb <= wall_rgb;
               if (bar2_on = '1') then
                   graph_rgb <= bar_rgb;
 			  elsif (bar1_on = '1') then
